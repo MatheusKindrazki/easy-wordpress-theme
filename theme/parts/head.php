54afdd0061b73d6ff08bfa463b4eb9a4bbead0bd
Matheus Kindrazki <matheuskindrazki@gmail.com>
2019-12-27 17:10:59 -0300

    estrutura gitignore

--- theme/parts/head.php
+++ theme/parts/head.php
@@ -9,15 +9,7 @@
 
 	<link rel="icon" type="image/png" href="<?php bloginfo('template_directory');?>/images/favicon.png"/>
 
-	<!-- jQuery -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<!-- Bootstrap -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<!-- Font Awesome -->
-	<script src="https://kit.fontawesome.com/c52cdfa652.js" crossorigin="anonymous"></script>
-	<!-- Owl Carousel Style -->
-	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/style/css/owl.carousel.min.css">
-	<!-- Theme Style -->
+	<!-- Theme Style Bundle -->
 	<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/style/css/style.css">
 
 </head>
