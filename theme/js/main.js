207a991e7f21f025c05ff973478e9c50cc37debe
Matheus Kindrazki <matheuskindrazki@gmail.com>
2019-12-27 18:00:54 -0300

    estrutura utilizando node_modules

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,1 +1,25 @@
-import "./custom/teste";
+/**
+ *
+ * As importações do boostrap são pesadas
+ * e muitos recursos não são utilizados
+ * o ideal é importar apelas o que será
+ * necessário.
+ * exemplo : import "bootstrap/js/dist/modal";
+ *
+ *  */
+
+// Owl carousel
+import "owl.carousel/dist/owl.carousel";
+
+/**
+ * Para manipulação do DOM, o Jquery pesa
+ * cerca de 40KB minificado. Utilizamos
+ * então o Cash Dom, uma alternativa de pesa
+ * cerca de 90% a menos com a mesma eficacia.
+ * Peso: 5KB
+ *
+ * Github: https://github.com/kenwheeler/cash
+ * Uso:  Importe o Cash nos arquivos que for
+ * utilizar
+ * Exemplo: import $ from "cash-dom";
+ */
